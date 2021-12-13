760ecb89d904383fa66edd8f35848154dffca7a9
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-12-13 21:15:36 +0500

    error

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiYXNhZGJlay1kZXYiLCJhIjoiY2t3dXlmcHVmMXV3MzJ2cG1qNmhkd3NiaiJ9.t85Qc70M8dLztbUNls1QaQ';
-    var map = new mapboxgl.Map({
+
+    navigator.geolocation.getCurrentPosition(successLocation,errorLocation,{
+        enableHighAccuracy:true
+    })
+    function successLocation(position){
+        console.log(position);
+    }
+
+var map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v11'
     });
