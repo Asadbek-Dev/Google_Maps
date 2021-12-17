76883741f361afbf8fc2151653f3954e11d17102
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-12-17 21:05:49 +0500

    changed

--- script.js
+++ script.js
@@ -5,12 +5,16 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYXNhZGJlay1kZXYiLCJhIjoiY2t3dXlmcHVmMXV3MzJ2c
     })
     function successLocation(position){
         console.log(position);
+        setupMAp(position.coords)
     }
-    function errorLocation(position){
-        console.log(position);
+    function errorLocation(){
+
+    }
+    function setupMAp(center){
+        var map = new mapboxgl.Map({
+                container: 'map',
+                style: 'mapbox://styles/mapbox/streets-v11',
+                center:center
+        });
     }
 
-var map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11'
-    });
